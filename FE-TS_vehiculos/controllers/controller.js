05ca377140f06db4253d9a0bd2159de3ea122ee8
Consultas BBDD Flights USA <>
2020-01-28 11:05:53 +0100

    Actualizo validaciones y estilos

--- FE-TS_vehiculos/controllers/controller.js
+++ FE-TS_vehiculos/controllers/controller.js
@@ -1,31 +1,41 @@
 "use strict";
-var mostraRtadoPaso1 = document.getElementById('mostraRtadoPaso1');
+var rtadoPlate = document.getElementById('rtadoPlate');
+var rtadoBrand = document.getElementById('rtadoBrand');
+var rtadoColor = document.getElementById('rtadoColor');
 var tituloRtado = document.getElementById('tituloRtado');
 var formPaso1 = document.getElementById('formPaso1');
 var formPaso2 = document.getElementById('formPaso2');
-var mostraRtadoPaso2A = document.getElementById('mostraRtadoPaso2A');
-var mostraRtadoPaso2B = document.getElementById('mostraRtadoPaso2B');
+var rueda1 = document.getElementById('rueda1');
+var rueda2 = document.getElementById('rueda2');
+var rueda3 = document.getElementById('rueda3');
+var rueda4 = document.getElementById('rueda4');
 var car;
 function createCar() {
-    var plate = document.getElementById('plate').value;
+    var plate = (document.getElementById('plate').value).toUpperCase();
     var brand = document.getElementById('brand').value;
     var color = document.getElementById('color').value;
     car = new Car(plate, color, brand);
-    // const regex : RegExp = /abc| ... |123/gi;
-    // console.log(regex.test(123));
-    if (plate.match(/^[a-z]{3}[0-9]{3}$/gi)) { // {6,} es la cantidad de caracteres    
+    var acumErroresCar = [];
+    var regExPlate = /^[0-9]{4}[a-z]{3}$/gi;
+    var plateOk = plate.match(regExPlate);
+    if (plateOk == null) {
+        acumErroresCar.push('- Ingrese un PLATE compuesto por 4 números y 3 letras');
+    }
+    if (car.plate == "" || car.brand == "" || car.color == "") {
+        acumErroresCar.push('- Todos los campos son obligatorios');
+    }
+    else if (plateOk != null && car.brand != "" && car.color != "") { // {6,} es la cantidad de caracteres    
         // acciones del paso 1
-        mostraRtadoPaso1.innerHTML = "Plate: " + car.plate
-            + "<br/>Brand: " + car.brand + "<br/>Color: " + car.color;
+        rtadoPlate.innerHTML = "Plate: " + car.plate;
+        rtadoBrand.innerHTML = "Marca: " + car.brand;
+        rtadoColor.innerHTML = "Color: " + car.color;
         tituloRtado.setAttribute("style", "visibility: visible;");
         ;
         formPaso1.setAttribute("style", "display:none;");
         formPaso2.setAttribute("style", "display:inline;");
-    }
-    else {
-        alert('Por favor ingrese un PLATE compuesto de 3 letras y 3 números');
         return;
     }
+    alert(acumErroresCar.join('\n'));
 }
 function addWheels() {
     // ¿se puede juntar en 1?
@@ -40,30 +50,35 @@ function addWheels() {
     var diamRueda4 = document.getElementById('diamRueda4').value;
     var todosLosDiametrosOr = [diamRueda1, diamRueda2, diamRueda3, diamRueda4];
     var todosLosDiametrosNum = []; //array vacio que recibirá los valores de diametro como numero
+    var acumErroresWheel = [];
     // Esta funcion convierte los diametros a numero y luego valida los datos
     for (var i = 0; i < 4; i++) {
         var diamValueNum = Number(todosLosDiametrosOr[i]); // convierte el input (que es siempre un string) en numero
-        if (isNaN(diamValueNum)) { //validar si ha ingresado un numero o un NaN
-            alert('El diámetro de RUEDA ' + (i + 1) + ' debe ser únicamente numérico');
-            return; //frena la funcion para que el usuario ingrese un diametro valido
+        if (todasLasMarcas[i] == "") {
+            acumErroresWheel.push('- Por favor complete la marca de la RUEDA ' + (i + 1));
         }
-        else if (diamValueNum < 0.4 || diamValueNum > 2) {
-            alert('Ingrese un diámetro entre 0.4 y 2 para RUEDA ' + (i + 1));
-            return;
+        if (isNaN(diamValueNum)) { //validar si ha ingresado un numero o un NaN
+            acumErroresWheel.push('- El diámetro de RUEDA ' + (i + 1) + ' debe ser únicamente numérico');
+        }
+        else if (diamValueNum <= 0.4 || diamValueNum >= 2) {
+            acumErroresWheel.push('- Ingrese un diámetro mayor que 0.4 y menor que 2 para RUEDA ' + (i + 1));
         }
         else {
             todosLosDiametrosNum.push(diamValueNum);
         }
+    }
+    if (acumErroresWheel && acumErroresWheel.length) {
+        alert(acumErroresWheel.join('\n'));
+        return;
     }
     // esta funcion suma las ruedas a Car
     for (var i = 0; i < 4; i++) {
         var rueda = new Wheel(todosLosDiametrosNum[i], todasLasMarcas[i]);
         car.addWheel(rueda);
     }
-    mostraRtadoPaso2A.innerHTML = "RUEDA 1: <br/>Diametro: " + car.wheels[0].diameter
-        + "<br/>Brand: " + car.wheels[0].brand + "<br/><br/>RUEDA 3: <br/>Diametro: " + car.wheels[2].diameter
-        + "<br/>Brand: " + car.wheels[2].brand;
-    mostraRtadoPaso2B.innerHTML = "RUEDA 2: <br/>Diametro: " + car.wheels[1].diameter
-        + "<br/>Brand: " + car.wheels[1].brand + "<br/><br/>RUEDA 4: <br/>Diametro: " + car.wheels[3].diameter
-        + "<br/>Brand: " + car.wheels[3].brand;
+    rueda1.innerHTML = "Marca: " + car.wheels[0].brand + "<br/>Diametro: " + car.wheels[0].diameter;
+    rueda2.innerHTML = "Marca: " + car.wheels[1].brand + "<br/>Diametro: " + car.wheels[1].diameter;
+    rueda3.innerHTML = "Marca: " + car.wheels[2].brand + "<br/>Diametro: " + car.wheels[2].diameter;
+    rueda4.innerHTML = "Marca: " + car.wheels[3].brand + "<br/>Diametro: " + car.wheels[3].diameter;
 }
+// todos los campos required
--- FE-TS_vehiculos/controllers/controller.ts
+++ FE-TS_vehiculos/controllers/controller.ts
@@ -1,34 +1,48 @@
-var mostraRtadoPaso1 = <HTMLElement>document.getElementById('mostraRtadoPaso1');
+var rtadoPlate = <HTMLElement>document.getElementById('rtadoPlate');
+var rtadoBrand = <HTMLElement>document.getElementById('rtadoBrand');
+var rtadoColor = <HTMLElement>document.getElementById('rtadoColor');
+
 var tituloRtado = <HTMLElement>document.getElementById('tituloRtado');
 var formPaso1 = <HTMLElement>document.getElementById('formPaso1');
 var formPaso2 = <HTMLElement>document.getElementById('formPaso2');
 
-var mostraRtadoPaso2A = <HTMLElement>document.getElementById('mostraRtadoPaso2A');
-var mostraRtadoPaso2B = <HTMLElement>document.getElementById('mostraRtadoPaso2B');
+var rueda1 = <HTMLElement>document.getElementById('rueda1');
+var rueda2 = <HTMLElement>document.getElementById('rueda2');
+var rueda3 = <HTMLElement>document.getElementById('rueda3');
+var rueda4 = <HTMLElement>document.getElementById('rueda4');
 
 var car:Car;
 
 
 function createCar(){
-    var plate = (<HTMLInputElement>document.getElementById('plate')).value;
+    var plate = ((<HTMLInputElement>document.getElementById('plate')).value).toUpperCase( );
     var brand = (<HTMLInputElement>document.getElementById('brand')).value;
     var color = (<HTMLInputElement>document.getElementById('color')).value;
 
     car = new Car(plate,color,brand);
 
-    // const regex : RegExp = /abc| ... |123/gi;
-    // console.log(regex.test(123));
-    if (plate.match(/^[a-z]{3}[0-9]{3}$/gi)) { // {6,} es la cantidad de caracteres    
+    var acumErroresCar: string[] = [];
+    
+    var regExPlate = /^[0-9]{4}[a-z]{3}$/gi;
+    var plateOk = plate.match(regExPlate);
+    if (plateOk == null) {
+        acumErroresCar.push('- Ingrese un PLATE compuesto por 4 números y 3 letras');
+    }
+
+    if (car.plate == "" || car.brand == "" || car.color == "") {
+        acumErroresCar.push('- Todos los campos son obligatorios');
+    } else if ( plateOk != null && car.brand != "" && car.color != "") { // {6,} es la cantidad de caracteres    
         // acciones del paso 1
-        mostraRtadoPaso1.innerHTML = "Plate: " + car.plate 
-        + "<br/>Brand: " + car.brand + "<br/>Color: " + car.color;
+        rtadoPlate.innerHTML = "Plate: " + car.plate;
+        rtadoBrand.innerHTML = "Marca: " + car.brand;
+        rtadoColor.innerHTML = "Color: " + car.color;
         tituloRtado.setAttribute("style", "visibility: visible;");;
         formPaso1.setAttribute("style", "display:none;");
         formPaso2.setAttribute("style", "display:inline;");
-    } else {
-        alert('Por favor ingrese un PLATE compuesto de 3 letras y 3 números');
-        return
+        return;
     } 
+
+    alert(acumErroresCar.join('\n'));
 }
 
 
@@ -49,19 +63,27 @@ function addWheels() {
     let todosLosDiametrosOr = [diamRueda1, diamRueda2, diamRueda3, diamRueda4];
     let todosLosDiametrosNum = []; //array vacio que recibirá los valores de diametro como numero
     
+    var acumErroresWheel: string[] = [];
+    
     // Esta funcion convierte los diametros a numero y luego valida los datos
     for (var i = 0; i < 4; i++) {  
         var diamValueNum = Number(todosLosDiametrosOr[i]); // convierte el input (que es siempre un string) en numero
+        if (todasLasMarcas[i] == "") {
+            acumErroresWheel.push('- Por favor complete la marca de la RUEDA ' + (i+1));
+        }
         if (isNaN(diamValueNum)) { //validar si ha ingresado un numero o un NaN
-            alert('El diámetro de RUEDA ' + (i+1) + ' debe ser únicamente numérico');
-            return; //frena la funcion para que el usuario ingrese un diametro valido
-        } else if (diamValueNum <0.4 || diamValueNum > 2) {
-            alert('Ingrese un diámetro entre 0.4 y 2 para RUEDA ' + (i+1));
-            return; 
+            acumErroresWheel.push('- El diámetro de RUEDA ' + (i+1) + ' debe ser únicamente numérico');
+        } else if (diamValueNum <= 0.4 || diamValueNum >= 2) {
+            acumErroresWheel.push('- Ingrese un diámetro mayor que 0.4 y menor que 2 para RUEDA ' + (i+1));
         } else {
             todosLosDiametrosNum.push(diamValueNum);
-        }
+        } 
     }
+
+    if (acumErroresWheel && acumErroresWheel.length) {
+        alert(acumErroresWheel.join('\n'));
+        return;
+    } 
 
     // esta funcion suma las ruedas a Car
     for (var i = 0; i < 4; i++) {   
@@ -69,11 +91,12 @@ function addWheels() {
         car.addWheel(rueda);
     }
 
-    mostraRtadoPaso2A.innerHTML = "RUEDA 1: <br/>Diametro: " + car.wheels[0].diameter 
-    + "<br/>Brand: " + car.wheels[0].brand + "<br/><br/>RUEDA 3: <br/>Diametro: " + car.wheels[2].diameter 
-    + "<br/>Brand: " + car.wheels[2].brand;
-
-    mostraRtadoPaso2B.innerHTML = "RUEDA 2: <br/>Diametro: " + car.wheels[1].diameter 
-    + "<br/>Brand: " + car.wheels[1].brand + "<br/><br/>RUEDA 4: <br/>Diametro: " + car.wheels[3].diameter 
-    + "<br/>Brand: " + car.wheels[3].brand;
+    rueda1.innerHTML = "Marca: " + car.wheels[0].brand + "<br/>Diametro: " + car.wheels[0].diameter;
+    rueda2.innerHTML = "Marca: " + car.wheels[1].brand + "<br/>Diametro: " + car.wheels[1].diameter;
+    rueda3.innerHTML = "Marca: " + car.wheels[2].brand + "<br/>Diametro: " + car.wheels[2].diameter;
+    rueda4.innerHTML = "Marca: " + car.wheels[3].brand + "<br/>Diametro: " + car.wheels[3].diameter;
 }
+
+
+
+// todos los campos required
